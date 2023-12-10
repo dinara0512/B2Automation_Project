cf2ca86421ff2cdde468478db14a9f5c75475dc4
Dina <515lincolnstreet@gmail.com>
2023-12-10 09:41:34 -0500

    new commit

--- src/test/java/com/loop/test/utilities/DocuportConstants.java
+++ src/test/java/com/loop/test/utilities/DocuportConstants.java
@@ -2,11 +2,17 @@ package com.loop.test.utilities;
 
 public class DocuportConstants {
     public static final String USERNAME_CLIENT = "b1g1_client@gmail.com";
-    public static final String ERROR_MESSAGE_FOR_EMPTY_PASSWORD ="" ;
+    public static final String ERROR_MESSAGE_FOR_EMPTY_PASSWORD = "";
 
+public static final String USERNAME_ADVISOR = "b1g1_advisor@gmail.com";
+        public static final String LOGO_DOCUPORT = "Docuport";
 
-    public static void main(String[] args) {
-    //    WebDriverFactory
+        public static final String VALID_PASSWORD = "Group3";
+        public static final String CLIENT_HOME_PAGE = "https://beta.docuport.app/ ";
 
+        public static final String DOCUPORT_PAGE = "https://beta.docuport.app/login";
+        public static final String RESET_PASSWORD_URL = "reset-password";
+        public static final String RESET_PASSWORD_MESSAGE = "Enter the email address associated with your account";
+
+        public static final String EMAIL_FOR_RESET_PASSWORD = "forgotpasswordg1@gmail.com";
     }
-}
